3d96db926ea2910b8428234ce36659e0003fec65
VadimSolodyuk <smr8306@gmail.com>
2025-09-24 20:34:28 +0500

    24.09.25

--- OOP_1.8.py
+++ OOP_1.8.py
@@ -1,52 +1,65 @@
-"""Время первого испытания. Представьте, что вы получили задание от заказчика. Вас просят реализовать простую имитацию локальной сети, состоящую из набора серверов, соединенных между собой через роутер.
+"""Время первого испытания.
+Представьте, что вы получили задание от заказчика. Вас просят реализовать
+простую имитацию локальной сети, состоящую из набора серверов, соединенных
+между собой через роутер.
 
-Каждый сервер может отправлять пакет любому другому серверу сети. Для этого у каждого есть свой уникальный IP-адрес. Для простоты - это просто целое (натуральное) число от 1 и до N, где N - общее число серверов. Алгоритм следующий. Предположим, сервер с IP = 2 собирается отправить пакет информации серверу с IP = 3. Для этого, он сначала отправляет пакет роутеру, а уже тот, смотрит на IP-адрес и пересылает пакет нужному узлу (серверу).
+Каждый сервер может отправлять пакет любому другому серверу сети.
+Для этого у каждого есть свой уникальный IP-адрес.
+Для простоты - это просто целое (натуральное) число от 1 и до N,
+где N - общее число серверов. Алгоритм следующий.
+Предположим, сервер с IP = 2 собирается отправить пакет информации 
+серверу с IP = 3. Для этого, он сначала отправляет пакет роутеру,
+а уже тот, смотрит на IP-адрес и пересылает пакет нужному узлу (серверу).
 
 Для реализации этой схемы программе предлагается объявить три класса:
-
 Server - для описания работы серверов в сети;
-Router - для описания работы роутеров в сети (в данной задаче полагается один роутер);
+Router - для описания работы роутеров в сети (в данной задаче полагается
+один роутер);
 Data - для описания пакета информации.
 
 Серверы будут создаваться командой:
-
 sv = Server()
-При этом, уникальный IP-адрес каждого сервера должен формироваться автоматически при создании нового экземпляра класса Server.
+При этом, уникальный IP-адрес каждого сервера должен формироваться 
+автоматически при создании нового экземпляра класса Server.
 
 Далее, роутер должен создаваться аналогичной командой:
-
 router = Router()
+
 А, пакеты данных, командой:
-
 data = Data(строка с данными, IP-адрес назначения)
-Для формирования и функционирования локальной сети, в классе Router должны быть реализованы следующие методы:
 
-link(server) - для присоединения сервера server (объекта класса Server) к роутеру (для простоты, каждый сервер соединен только с одним роутером);
-unlink(server) - для отсоединения сервера server (объекта класса Server) от роутера;
-send_data() - для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам (после отправки буфер должен очищаться).
+Для формирования и функционирования локальной сети, в классе Router
+должны быть реализованы следующие методы:
+link(server) - для присоединения сервера server (объекта класса Server)
+к роутеру (для простоты, каждый сервер соединен только с одним роутером);
+unlink(server) - для отсоединения сервера server (объекта класса Server)
+от роутера;
+send_data() - для отправки всех пакетов (объектов класса Data) из буфера
+роутера соответствующим серверам (после отправки буфер должен очищаться).
 
 И одно обязательное локальное свойство (могут быть и другие свойства):
-
-buffer - список для хранения принятых от серверов пакетов (объектов класса Data).
+buffer - список для хранения принятых от серверов пакетов (объектов 
+класса Data).
 
 Класс Server должен содержать свой набор методов:
-
-send_data(data) - для отправки информационного пакета data (объекта класса Data) с указанным IP-адресом получателя (пакет отправляется роутеру и сохраняется в его буфере - локальном свойстве buffer);
-get_data() - возвращает список принятых пакетов (если ничего принято не было, то возвращается пустой список) и очищает входной буфер;
+send_data(data) - для отправки информационного пакета data (объекта
+класса Data) с указанным IP-адресом получателя (пакет отправляется 
+роутеру и сохраняется в его буфере - локальном свойстве buffer);
+get_data() - возвращает список принятых пакетов (если ничего принято
+не было, то возвращается пустой список) и очищает входной буфер;
 get_ip() - возвращает свой IP-адрес.
 
 Соответственно в объектах класса Server должны быть локальные свойства:
-
 buffer - список принятых пакетов (объекты класса Data, изначально пустой);
 ip - IP-адрес текущего сервера.
 
-Наконец, объекты класса Data должны содержать два следующих локальных свойства:
-
+Наконец, объекты класса Data должны содержать два следующих локальных
+свойства:
 data - передаваемые данные (строка);
 ip - IP-адрес назначения.
 
-Пример использования этих классов (эти строчки в программе писать не нужно):
-
+Пример использования этих классов (эти строчки в программе писать
+не нужно):
 router = Router()
 sv_from = Server()
 sv_from2 = Server()
@@ -62,89 +75,101 @@ sv_to.send_data(Data("Hi", sv_from.get_ip()))
 router.send_data()
 msg_lst_from = sv_from.get_data()
 msg_lst_to = sv_to.get_data()
-Ваша задача реализовать классы Router, Server и Data в соответствии с приведенным техническим заданием (ТЗ). Что-либо выводить на экран не нужно.
+Ваша задача реализовать классы Router, Server и Data в соответствии
+с приведенным техническим заданием (ТЗ). Что-либо выводить на экран
+не нужно.
 """
 
 
 class Router:
     def __init__(self):
-       self.__buffer = []
-       self.__servers = {}
-       
-    def __check_link_server(self, server):
-        return server.get_ip() in self.__servers
-    
+        self.buffer = []
+        self.servers = {}
+        
+    # Присоединение объекта класса Server к роутеру (для простоты, 
+    # каждый сервер соединен только с одним роутером)
     def link(self, server):
-        if not self.__check_link_server(server):
-            server.router = self
-            self.__servers[server.get_ip()] = server
-    
+        if not self.__is_linked(server):
+            self.servers[server.get_ip()] = server
+            server.link_router(self)
+            
+    def __is_linked(self, server):
+        return server.get_ip() in self.servers
+
+    # Отсоединение сервера server (объекта класса Server) от роутера
     def unlink(self, server):
-        if self.__check_link_server(server):
-            del self.__servers[server.get_ip()]
-            server.router = None
-           
-    def receive_data(self, data):
-        self.__buffer.append(data)       
+        if self.__is_linked(server):
+            del self.servers[server.get_ip()]
+            server.unlink_router()
+
+    def add_data(self, data):
+        if type(data) != Data:
+            raise ValueError
+        self.buffer.append(data) 
     
+    # Отправка всех пакетов (объектов класса Data) из буфера
+    # роутера соответствующим серверам (после отправки буфер должен
+    # очищаться)
     def send_data(self):
-        for data in self.__buffer:
-            data_ip = data.get_ip()
-            if data_ip not in self.__servers:
-                continue
-            self.__servers[data_ip].receive_data(data)
-        self.__buffer.clear()
+        while len(self.buffer) != 0:
+            data = self.buffer.pop()
+            ip_server = data.get_ip()
+            if ip_server in self.servers:
+                self.servers[ip_server].add_data(data)
 
 
 class Server:
-    servers_ip = 1
-        
-    # def __new__(cls, *args, **kwargs):
-    #     cls.servers_ip += 1
-    #     return super().__new__(cls)
-      
+    NUMBER_SERVERS = 0
+    
     def __init__(self):
-       self.__ip = Server.servers_ip
-       Server.servers_ip += 1
-       
-       self.router = None
-       self.__buffer = [] 
+        self.ip = self.__set_ip()
+        self.buffer = []
+        self.router = None
+        
+    @classmethod
+    def __set_ip(cls):
+        cls.NUMBER_SERVERS += 1
+        return cls.NUMBER_SERVERS
     
-    def receive_data(self, data):
-        self.__buffer.append(data)       
-
-    def get_ip(self):
-        return self.__ip
-    
-    def get_data(self):
-        if len(self.__buffer) == 0:
-            return []
-        lst_data = [*self.__buffer]
-        self.__buffer.clear() 
-        return lst_data
-    
+    # Для отправки информационного пакета data (объекта класса Data)
+    # с указанным IP-адресом получателя (пакет отправляется 
+    # роутеру и сохраняется в его буфеsре - локаьном свойстве buffer)
     def send_data(self, data):
         if self.router:
-            self.router.receive_data(data)
-   
+            self.router.add_data(data)
+
+    # Возвращает список принятых пакетов (если ничего принятоне было,
+    # то возвращается пустой список) и очищает входной буфер
+    def get_data(self):
+        buffer = self.buffer[:]
+        self.buffer.clear()
+        return buffer
     
-class Data:
-    def __init__(self, data, ip):
-        if type(data) != str:
-            raise ValueError("некорректное поле data")
-        if type(ip) == str and not ip.isdigit():
-            raise ValueError("некорректное поле ip")
-        elif int(ip) < 1:
-            raise ValueError("некорректное поле ip")
+    def add_data(self, data):
+        self.buffer.append(data) 
+    
+    def link_router(self, router):
+        if type(router) != Router:
+            raise ValueError
+        self.router = router
         
+    def unlink_router(self):
+        self.router = None
+
+    def get_ip(self):
+        return self.ip
+    
+
+# Описания пакета информации
+class Data:
+    def __init__(self, data, to_ip):
         self.data = data
-        self.__ip = int(ip)
+        self.to_ip = to_ip
         
     def get_ip(self):
-        return self.__ip
-    
-    
-    
+        return self.to_ip
+
+
 router = Router()
 sv_from = Server()
 sv_from2 = Server()
@@ -160,5 +185,3 @@ sv_to.send_data(Data("Hi", sv_from.get_ip()))
 router.send_data()
 msg_lst_from = sv_from.get_data()
 msg_lst_to = sv_to.get_data()
-
-
