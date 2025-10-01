a4e094491580c1f34aedfba991c3130ac2b793ac
VadimSolodyuk <smr8306@gmail.com>
2025-10-01 21:45:28 +0500

    OOP_2.1_10: TODO check_email()

--- OOP_2.1_10 copy.py
+++ OOP_2.1_10 copy.py
@@ -1,29 +1,77 @@
 """Объявите класс EmailValidator для проверки корректности email-адреса. 
-Необходимо запретить создание объектов этого класса: при создании экземпляров должно
+Необходимо запретить создание объектов этого класса: при создании 
+экземпляров должно
 возвращаться значение None, например:
 em = EmailValidator() # None
 
 В самом классе реализовать следующие методы класса (@classmethod):
+
 get_random_email(cls) - для генерации случайного email-адреса по формату:
 xxxxxxx...xxx@gmail.com, где x - любой допустимый символ в email
 (латинский буквы, цифры, символ подчеркивания и точка);
-check_email(cls, email) - возвращает True, если email записан верно и False - в противном случае.
 
+check_email(cls, email) - возвращает True, если email записан верно
+и False - в противном случае.
 Корректность строки email определяется по следующим критериям:
-- допустимые символы: латинский алфавит, цифры, символы подчеркивания, точки и собачка @ (одна);
+- допустимые символы: латинский алфавит, цифры, символы подчеркивания,
+точки и собачка @ (одна);
 - длина email до символа @ не должна превышать 100 (сто включительно);
 - длина email после символа @ не должна быть больше 50 (включительно);
 - после символа @ обязательно должна идти хотя бы одна точка;
 - не должно быть двух точек подряд.
 
 Также в классе нужно реализовать приватный статический метод класса:
+
 is_email_str(email) - для проверки типа переменной email, если строка,
 то возвращается значение True, иначе - False.
-Метод is_email_str() следует использовать в методе check_email() перед проверкой корректности email.
-Если параметр email не является строкой, то check_email() возвращает False.
+Метод is_email_str() следует использовать в методе check_email() перед
+проверкой корректности email.
+Если параметр email не является строкой, то check_email() возвращает
+False.
 
-Пример использования класса EmailValidator (эти строчки в программе писать не нужно):
+Пример использования класса EmailValidator (эти строчки в программе
+писать не нужно):
 res = EmailValidator.check_email("sc_lib@list.ru") # True
 res = EmailValidator.check_email("sc_lib@list_ru") # False
-P.S. В программе требуется объявить только класс. На экран ничего выводить не нужно.
+P.S. В программе требуется объявить только класс. На экран ничего
+выводить не нужно.
 """
+
+from string import ascii_letters, digits
+
+
+class EmailValidator:
+    CORRECT_CHARS = ascii_letters + digits + '_.'
+    length_mailbox_name = 100
+    length_domain = 50
+    domain_gmail = "gmail.com"
+    
+    def __new__(cls):
+        return None
+    
+    @classmethod
+    def get_random_email(cls):
+        pass
+    
+    @classmethod
+    def check_email(cls, email):
+        if not cls.__is_email_str(email):
+            return False
+        elif (not set(email) < set(cls.CORRECT_CHARS)
+              or ):
+            return False
+        
+    @classmethod
+    def __check_mailbox_name(cls, mailbox_name):
+            
+            
+    
+    @staticmethod
+    def __is_email_str(email):
+        return isinstance(email, str)
+        
+    
+    
+    
+    
+# print(EmailValidator())    
