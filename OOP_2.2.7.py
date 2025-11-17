67eef166c3618a404a33d8808d9809e11e38b677
VadimSolodyuk <129878860+VadimSolodyuk@users.noreply.github.com>
2025-11-17 17:26:20 +0500

    OOP_2.2.7: todo

--- OOP_2.2.7.py
+++ OOP_2.2.7.py
@@ -3,15 +3,56 @@
 
 
 class RadiusVector2D:
+    MIN_COORD = -100
+    MAX_COORD = 1024
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
     @property
-    def coord_x(self):
+    def x(self):
         return self.__x
     
-    @coord_x.setter
-    def coord_x(self, value):
-        if self.__check_coords_value(value): # TODO
+    @x.setter
+    def x(self, value):
+        if not self.__check_coord_value(value):
+            # if self.__x:
+            #     return
+            self.__x = 0
+        else:
             self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        if not self.__check_coord_value(value):
+            # if self.__y:
+            #     return
+            self.__y = 0
+        else:
+            self.__y = value
+
+    @classmethod
+    def __check_coord_value(cls, value):
+        if not ((type(value) == int or type(value) == float)
+                and cls.MIN_COORD <= value <= cls.MAX_COORD):
+            return False
+        else:
+            return True
+        
+    @staticmethod
+    def norm2(vector):
+        return vector.x**2 + vector.y**2
+    
+
+r1 = RadiusVector2D()
+print(r1.x, r1.y)
+r2 = RadiusVector2D(1, -101)
+print(r2.x, r2.y)
+r2.x = '5'
+print(r2.x, r2.y)
+print(r2.__dict__)
